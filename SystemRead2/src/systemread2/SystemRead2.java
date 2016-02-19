17192e1feef42bb6f234c8abbd205484e24401f9
Eightgate <Eightgate@koalarainbows>
2016-02-19 09:38:10 -0500

    2nd commit

---
+++ SystemRead2/src/systemread2/SystemRead2.java
@@ -0,0 +1,23 @@
+/*
+ *   Project:   ReadMySystem2
+ *    Author:   Caroline Ganier
+ *      Date:   2/18/2016
+ *
+ *  Description:
+ *          
+ *              This is a small program to practice 3 skills.
+ *                  (1) building a fast, but usable GUI
+ *                  (2) making an OS call for environmental variables
+ *                  (3) setting up and properly utiliing Git with my NetBeans 
+ *                      built in Git features
+ *
+ */
+package systemread2;
+
+public class SystemRead2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
